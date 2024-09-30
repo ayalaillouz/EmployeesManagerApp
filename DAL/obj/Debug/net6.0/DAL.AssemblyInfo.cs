f38523bcfe4b664b723d62a43b8e8adfff54d0d8
ayalaillouz <a0534195130@gmail.com>
2024-09-30 21:49:51 +0300

    sss

--- DAL/obj/Debug/net6.0/DAL.AssemblyInfo.cs
+++ DAL/obj/Debug/net6.0/DAL.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DAL")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1adb8b511b2bd3fb031b4b1099c925c241ebb5e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("DAL")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DAL")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
